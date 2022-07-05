89533bf3780d15978afd0891807811295270aef8
Chris Gaunt <chris.gaunt@gmail.com>
2022-07-05 14:01:46 +0100

    Add JS

---
+++ docs/javascripts/extra.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){ 
+    var urlHash = window.location.href.split("#")[1];
+    var element = document.getElementById(urlHash);
+    if (urlHash && typeof(element) != 'undefined' && element != null) {
+        var elementTop = element.offsetTop-128;
+        console.log(elementTop);
+        window.scrollTo(0, elementTop);
+    }
+}, false);
